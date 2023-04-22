2f87a25acc8ada35c2d92c10662dfcee7cf3a209
Stanislav Cherevkov <web--dev@hotmail.com>
2023-04-22 18:12:13 +0300

    feat: updated structure and added functional 'Tab', 'Space', 'Shift', 'CapsLock', 'Ctrl' and 'Alt' keys

--- js/app.js
+++ js/app.js
@@ -2,70 +2,72 @@ if (!sessionStorage['lang']) {
   sessionStorage['lang'] = 'en';
 }
 
-let lang = sessionStorage['lang'];
-
 class Keyboard {
+  constructor () {
+    this.keyboardRender();
+  }
+
   keysInfo = [
               [
-                ['keyboard-key keyboard-key--backquote under-shift under-capslock', '~', 'Ë', 'Backquote', '`', 'ё'], 
-                ['keyboard-key under-shift', '!', '', 'Digit1', '1', '1'], 
-                ['keyboard-key under-shift', '@', '', 'Digit2', '2', '2'], 
-                ['keyboard-key under-shift', '#', '', 'Digit3', '3', '3'], 
-                ['keyboard-key under-shift', '$', '', 'Digit4', '4', '4'], 
-                ['keyboard-key under-shift', '%', '', 'Digit5', '5', '5'], 
-                ['keyboard-key under-shift', '^', '', 'Digit6', '6', '6'], 
-                ['keyboard-key under-shift', '&', '', 'Digit7', '7', '7'], 
-                ['keyboard-key under-shift', '*', '', 'Digit8', '8', '8'], 
-                ['keyboard-key under-shift', '(', '', 'Digit9', '9', '9'], 
-                ['keyboard-key under-shift', ')', '', 'Digit0', '0', '0'], 
-                ['keyboard-key under-shift', '_', '', 'Minus', '-', '-'], 
-                ['keyboard-key under-shift', '+', '', 'Equal', '=', '='], 
+                ['keyboard-key keyboard-key--backquote under-shift under-capslock lang', '~', 'Ë', 'Backquote', '`', 'ё'], 
+                ['keyboard-key under-shift', '!', '!', 'Digit1', '1', '1'], 
+                ['keyboard-key under-shift', '@', '@', 'Digit2', '2', '2'], 
+                ['keyboard-key under-shift', '#', '#', 'Digit3', '3', '3'], 
+                ['keyboard-key under-shift', '$', '$', 'Digit4', '4', '4'], 
+                ['keyboard-key under-shift', '%', '%', 'Digit5', '5', '5'], 
+                ['keyboard-key under-shift', '^', '^', 'Digit6', '6', '6'], 
+                ['keyboard-key under-shift', '&', '&', 'Digit7', '7', '7'], 
+                ['keyboard-key under-shift', '*', '*', 'Digit8', '8', '8'], 
+                ['keyboard-key under-shift', '(', '(', 'Digit9', '9', '9'], 
+                ['keyboard-key under-shift', ')', ')', 'Digit0', '0', '0'], 
+                ['keyboard-key under-shift', '_', '_', 'Minus', '-', '-'], 
+                ['keyboard-key under-shift', '+', '+', 'Equal', '=', '='], 
                 ['keyboard-key keyboard-key--functional keyboard-key--backspace', '', '', 'Backspace', 'Backspace', 'Backspace'], 
               ], 
               [
                 ['keyboard-key keyboard-key--functional', '', '', 'Tab', 'Tab', 'Tab'], 
-                ['keyboard-key under-shift under-capslock', 'Q', 'Й', 'KeyQ', 'q', 'й'], 
-                ['keyboard-key under-shift under-capslock', 'W', 'Ц', 'KeyW', 'w', 'ц'], 
-                ['keyboard-key under-shift under-capslock', 'E', 'У', 'KeyE', 'e', 'у'], 
-                ['keyboard-key under-shift under-capslock', 'R', 'К', 'KeyR', 'r', 'к'], 
-                ['keyboard-key under-shift under-capslock', 'T', 'Е', 'KeyT', 't', 'е'], 
-                ['keyboard-key under-shift under-capslock', 'Y', 'Н', 'KeyY', 'y', 'н'], 
-                ['keyboard-key under-shift under-capslock', 'U', 'Г', 'KeyU', 'u', 'г'], 
-                ['keyboard-key under-shift under-capslock', 'I', 'Ш', 'KeyI', 'i', 'ш'], 
-                ['keyboard-key under-shift under-capslock', 'O', 'Щ', 'KeyO', 'o', 'щ'], 
-                ['keyboard-key under-shift under-capslock', 'P', 'З', 'KeyP', 'p', 'з'], 
-                ['keyboard-key under-shift under-capslock', '{', 'Х', 'BracketLeft', '[', 'х'], 
+                ['keyboard-key under-shift under-capslock lang', 'Q', 'Й', 'KeyQ', 'q', 'й'], 
+                ['keyboard-key under-shift under-capslock lang', 'W', 'Ц', 'KeyW', 'w', 'ц'], 
+                ['keyboard-key under-shift under-capslock lang', 'E', 'У', 'KeyE', 'e', 'у'], 
+                ['keyboard-key under-shift under-capslock lang', 'R', 'К', 'KeyR', 'r', 'к'], 
+                ['keyboard-key under-shift under-capslock lang', 'T', 'Е', 'KeyT', 't', 'е'], 
+                ['keyboard-key under-shift under-capslock lang', 'Y', 'Н', 'KeyY', 'y', 'н'], 
+                ['keyboard-key under-shift under-capslock lang', 'U', 'Г', 'KeyU', 'u', 'г'], 
+                ['keyboard-key under-shift under-capslock lang', 'I', 'Ш', 'KeyI', 'i', 'ш'], 
+                ['keyboard-key under-shift under-capslock lang', 'O', 'Щ', 'KeyO', 'o', 'щ'], 
+                ['keyboard-key under-shift under-capslock lang', 'P', 'З', 'KeyP', 'p', 'з'], 
+                ['keyboard-key under-shift under-capslock lang', '{', 'Х', 'BracketLeft', '[', 'х'], 
                 ['keyboard-key under-shift under-capslock', '}', 'Ъ', 'BracketRight', ']', 'ъ'], 
                 ['keyboard-key under-shift', '|', '/', 'Backslash', '\\', '\\'], 
                 ['keyboard-key keyboard-key--functional', '', '', 'Delete', 'Del', 'Del'], 
               ], 
               [
                 ['keyboard-key keyboard-key--functional keyboard-key--capslock', '', '', 'CapsLock', 'CapsLock', 'CapsLock'], 
-                ['keyboard-key under-shift under-capslock', 'A', 'Ф', 'KeyA', 'a', 'ф'], 
-                ['keyboard-key under-shift under-capslock', 'S', 'Ы', 'KeyS', 's', 'ы'], 
-                ['keyboard-key under-shift under-capslock', 'D', 'В', 'KeyD', 'd', 'в'], 
-                ['keyboard-key under-shift under-capslock', 'F', 'А', 'KeyF', 'f', 'а'], 
-                ['keyboard-key under-shift under-capslock', 'G', 'П', 'KeyG', 'g', 'п'], 
-                ['keyboard-key under-shift under-capslock', 'H', 'Р', 'KeyH', 'h', 'р'], 
-                ['keyboard-key under-shift under-capslock', 'J', 'О', 'KeyJ', 'j', 'о'], 
-                ['keyboard-key under-shift under-capslock', 'K', 'Л', 'KeyK', 'k', 'л'], 
-                ['keyboard-key under-shift under-capslock', 'L', 'Д', 'KeyL', 'l', 'д'], 
-                ['keyboard-key under-shift under-capslock', ':', 'Ж', 'Semicolon', ';', 'ж'], 
-                ['keyboard-key under-shift under-capslock', '"', 'Э', 'Quote', '\'', 'э'], 
+                ['keyboard-key under-shift under-capslock lang', 'A', 'Ф', 'KeyA', 'a', 'ф'], 
+                ['keyboard-key under-shift under-capslock lang', 'S', 'Ы', 'KeyS', 's', 'ы'], 
+                ['keyboard-key under-shift under-capslock lang', 'D', 'В', 'KeyD', 'd', 'в'], 
+                ['keyboard-key under-shift under-capslock lang', 'F', 'А', 'KeyF', 'f', 'а'], 
+                ['keyboard-key under-shift under-capslock lang', 'G', 'П', 'KeyG', 'g', 'п'], 
+                ['keyboard-key under-shift under-capslock lang', 'H', 'Р', 'KeyH', 'h', 'р'], 
+                ['keyboard-key under-shift under-capslock lang', 'J', 'О', 'KeyJ', 'j', 'о'], 
+                ['keyboard-key under-shift under-capslock lang', 'K', 'Л', 'KeyK', 'k', 'л'], 
+                ['keyboard-key under-shift under-capslock lang', 'L', 'Д', 'KeyL', 'l', 'д'], 
+                ['keyboard-key under-shift under-capslock lang', ':', 'Ж', 'Semicolon', ';', 'ж'], 
+                ['keyboard-key under-shift under-capslock lang', '"', 'Э', 'Quote', '\'', 'э'], 
                 ['keyboard-key keyboard-key--functional keyboard-key--enter', '', '', 'Enter', 'Enter', 'Enter'], 
               ], 
               [
                 ['keyboard-key keyboard-key--functional keyboard-key--shift', '', '', 'ShiftLeft', 'Shift', 'Shift'], 
-                ['keyboard-key under-shift under-capslock', 'Z', 'Я', 'KeyZ', 'z', 'я'], 
-                ['keyboard-key under-shift under-capslock', 'X', 'Ч', 'KeyX', 'x', 'ч'], 
-                ['keyboard-key under-shift under-capslock', 'C', 'С', 'KeyC', 'c', 'с'], 
-                ['keyboard-key under-shift under-capslock', 'V', 'М', 'KeyV', 'v', 'м'], 
-                ['keyboard-key under-shift under-capslock', 'B', 'И', 'KeyB', 'b', 'и'], 
-                ['keyboard-key under-shift under-capslock', 'N', 'Т', 'KeyN', 'n', 'т'], 
-                ['keyboard-key under-shift under-capslock', 'M', 'Ь', 'KeyM', 'm', 'ь'], 
-                ['keyboard-key under-shift under-capslock', '&lt;', 'Б', 'Comma', ',', 'б'], 
-                ['keyboard-key under-shift under-capslock', '&gt;', 'Ю', 'Period', '.', 'ю'], 
-                ['keyboard-key under-shift', '?', ',', 'Slash', '/', '.'], 
+                ['keyboard-key under-shift under-capslock lang', 'Z', 'Я', 'KeyZ', 'z', 'я'], 
+                ['keyboard-key under-shift under-capslock lang', 'X', 'Ч', 'KeyX', 'x', 'ч'], 
+                ['keyboard-key under-shift under-capslock lang', 'C', 'С', 'KeyC', 'c', 'с'], 
+                ['keyboard-key under-shift under-capslock lang', 'V', 'М', 'KeyV', 'v', 'м'], 
+                ['keyboard-key under-shift under-capslock lang', 'B', 'И', 'KeyB', 'b', 'и'], 
+                ['keyboard-key under-shift under-capslock lang', 'N', 'Т', 'KeyN', 'n', 'т'], 
+                ['keyboard-key under-shift under-capslock lang', 'M', 'Ь', 'KeyM', 'm', 'ь'], 
+                ['keyboard-key under-shift under-capslock lang', '&lt;', 'Б', 'Comma', ',', 'б'], 
+                ['keyboard-key under-shift under-capslock lang', '&gt;', 'Ю', 'Period', '.', 'ю'], 
+                ['keyboard-key under-shift lang', '?', ',', 'Slash', '/', '.'], 
                 ['keyboard-key keyboard-key--functional keyboard-key--arrow-up', '', '', 'ArrowUp', '⬆', '⬆'], 
                 ['keyboard-key keyboard-key--functional keyboard-key--shift', '', '', 'ShiftRight', 'Shift', 'Shift'], 
               ], 
@@ -82,13 +84,13 @@ class Keyboard {
               ]
             ];
 
-  functionalKeys = [46, 17, 9, 20, 13, 16, 8, 18, 91, 93, 37, 38, 39, 40, 32];
+  functionalKeys = ['ControlRight', 'ControlLeft', 'MetaLeft', 'ArrowRight', 'AltLeft', 'Space', 'ArrowLeft', 'AltRight', 'ArrowDown', 'ArrowUp', 'ShiftRight', 'ShiftLeft', 'CapsLock', 'Enter', 'Tab', 'Delete', 'Backspace'];
   notCapsLockEnglish = ['BracketLeft', 'BracketRight', 'Semicolon', 'Quote', 'Comma', 'Period'];
   outputText = '';
-
-  constructor () {
-    this.keyboardRender();
-  }
+  capsLockIsPressed = false;
+  ctrlLeftIsPressed = false;
+  altLeftIsPressed = false;
+  lang = sessionStorage['lang'];
 
   keyboardRender () {
     const body = document.getElementsByTagName('body');
@@ -109,7 +111,7 @@ class Keyboard {
     keyboard.className = 'keyboard';
     this.output.className = 'keyboard-output';
     this.output.id = 'keyboard-output';
-    this.output.setAttribute('rows', '8');
+    this.output.setAttribute('rows', '10');
     keyboardBody.className = 'keyboard-body';
     keyboardBody.id = 'keyboard';
     footer.className = 'footer';
@@ -144,7 +146,7 @@ class Keyboard {
           key.setAttribute('data-value-en', htmlValueEn);
           key.setAttribute('data-value-ru', htmlValueRu);
         }
-        key.innerHTML = (lang === 'en') ? htmlValueEn : htmlValueRu;
+        key.innerHTML = (this.lang === 'en') ? htmlValueEn : htmlValueRu;
         keyboardRow.append(key);
       }
       keyboardBody.append(keyboardRow);
@@ -153,6 +155,7 @@ class Keyboard {
       this.keysUnderShift = document.getElementsByClassName('under-shift');
       this.keysUnderCapslock = document.getElementsByClassName('under-capslock');
       this.activeKeys = document.getElementsByClassName('active');
+      this.langKeys = document.getElementsByClassName('lang');
     }
   }
   
@@ -160,44 +163,72 @@ class Keyboard {
     return (this.functionalKeys.includes(keyCode)) ? true : false;
   }
   
-  mouseDown () {
-
-  }
-
-  mouseUp () {
-    
-  }
-  
-  keyDown () {
+  keyDown (key) {
     this.output.focus();
-    this.CurrentKey.classList.add('active');
-    if (this.CurrentKey.id === 'ShiftRight' || this.CurrentKey.id === 'ShiftLeft') {
-      this.keyShiftPressed('down');
+
+    if (!this.isFunctionalKey(key.id)) {
+      this.changeOutput(key.innerHTML);
+    } else {
+      if (key.id === 'Space') {
+        this.changeOutput(' ');
+      } else if ((key.id === 'ShiftRight' || key.id === 'ShiftLeft') && this.capsLockIsPressed === false) {
+        this.keyShiftPressed('down');
+      } else if (key.id === 'Tab') {
+        this.changeOutput('  ');
+      } else if (key.id === 'CapsLock') {
+        if (key.classList.contains('active')) {
+          this.keyShiftPressed('up');
+          key.classList.remove('active');
+          this.capsLockIsPressed = false;
+          return;
+        } else {
+          this.capsLockIsPressed = true;
+          this.keyShiftPressed('down');
+        }
+      } else if (key.id === 'ControlLeft') {
+        this.ctrlLeftIsPressed = true;
+      } else if (key.id === 'AltLeft') {
+        this.altLeftIsPressed = true;
+      } 
     }
+    key.classList.add('active');
   }
   
-  keyUp () {
+  keyUp (key) {
+    this.output.focus();
+
+    if ((key.id === 'ShiftRight' || key.id === 'ShiftLeft') && this.capsLockIsPressed === false) {
+      this.keyShiftPressed('up');
+    } else if (key.id === 'CapsLock' && this.capsLockIsPressed === true) {
+      this.capsLockIsPressed = false;
+      this.keyShiftPressed('up');
+    } else if (key.id === 'ControlLeft') {
+      this.ctrlLeftIsPressed = false;
+    } else if (key.id === 'AltLeft') {
+      this.altLeftIsPressed = false;
+    } 
+    
     setTimeout (() => {
       for (let i = 0; i < this.activeKeys.length; i++) {
+        if (this.activeKeys[i].id === 'CapsLock' && this.capsLockIsPressed === true) {
+          continue;
+        }
         this.activeKeys[i].classList.remove('active');
       };
     }, "200");
-    if (this.CurrentKey.id === 'ShiftRight' || this.CurrentKey.id === 'ShiftLeft') {
-      this.keyShiftPressed('up');
-    }
   }
 
-  keyShiftPressed(event) {
+  keyShiftPressed(direction) {
     for (let i = 0; i < this.keysUnderShift.length; i++) {
-      const dataAttr = (event === 'down') ? 'data-shift-' + lang : 'data-value-' + lang;
+      const dataAttr = (direction === 'down') ? 'data-shift-' + this.lang : 'data-value-' + this.lang;
       const keyValue = this.keysUnderShift[i].getAttribute(dataAttr);
       this.keysUnderShift[i].innerHTML = keyValue;
     };
   }
   
-  changeOutput (text) {
-    this.outputText += text;
-    output.innerHTML = this.outputText;
+  changeOutput (keyValue) {
+    this.outputText += keyValue;
+    this.output.innerHTML = this.outputText;
   }
 }
 
@@ -205,19 +236,33 @@ const keyboard = new Keyboard();
 
 for (let i = 0; i < keyboard.allKeys.length; i++) {
   keyboard.allKeys[i].addEventListener('mousedown', function () {
-    keyboard.mouseDown();
+    keyboard.keyDown(this);
   });
   keyboard.allKeys[i].addEventListener('mouseup', function () {
-    keyboard.mouseUp();
+    if (keyboard.allKeys[i].id !== 'CapsLock') {
+      keyboard.keyUp(this);
+    }
   });
 };
 
 document.addEventListener("keydown", function (event) {
-  keyboard.CurrentKey = document.getElementById(event.code);
-  keyboard.keyDown();
+  event.preventDefault();
+  const currentKey = document.getElementById(event.code);
+  if ( (event.code === 'AltLeft' && keyboard.ctrlLeftIsPressed === true) || (event.code === 'ControlLeft' && keyboard.altLeftIsPressed === true) ) {
+    sessionStorage['lang'] = (keyboard.lang === 'en') ? 'ru' : 'en';
+    keyboard.lang = sessionStorage['lang'];
+    
+    dataAttr = (keyboard.capsLockIsPressed === false) ? 'data-value-' + keyboard.lang : 'data-shift-' + keyboard.lang;
+    for (let i = 0; i < keyboard.langKeys.length; i++) {
+      keyboard.langKeys[i].innerHTML = keyboard.langKeys[i].getAttribute(dataAttr);
+    }
+  }
+  keyboard.keyDown(currentKey);
+
 });
 
 document.addEventListener("keyup", function (event) {
-  keyboard.CurrentKey = document.getElementById(event.code);
-  keyboard.keyUp();
+  event.preventDefault();
+  const currentKey = document.getElementById(event.code);
+  keyboard.keyUp(currentKey);
 });
